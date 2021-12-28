46abda8bab5e436efd7cd9e4df822a82fdbf54d6
- <=>
2021-12-28 17:04:14 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(2,3,4);
+        System.out.println(triangle);
+    }
+}
